00c418729e694bcafdd6923dd636723fccb90478
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-06-30 10:45:31 +0700

    Initialize develop branch

--- Git_Practice/src/App.tsx
+++ Git_Practice/src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Main branch
-    </h1>
-  )
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Main branch</h1>
+			<p>This is develop branch</p>
+		</>
+	);
 }
