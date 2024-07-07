96035ca69cbedc32162bae923b3f14e6e0d754d2
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-07-07 20:38:21 +0700

    Add new code :))

--- Git_Practice/src/App.tsx
+++ Git_Practice/src/App.tsx
@@ -3,6 +3,7 @@ export default function App() {
 		<>
 			<h1 className="text-3xl font-bold underline">Main branch</h1>
 			<p>This is develop branch</p>
+			<p>New code from develop branch</p>
 		</>
 	);
 }
